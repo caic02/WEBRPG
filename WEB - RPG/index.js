0abb4a4b4ede3a6f602585420c7caf61b1feec28
caic02 <43590880+caic02@users.noreply.github.com>
2024-10-31 08:06:17 -0300

    Add files via upload

---
+++ WEB - RPG/index.js
@@ -0,0 +1,60 @@
+"use strict";
+class Heroi {
+    constructor(nome, hpMaximo) {
+        this.nome = nome;
+        this.hpMaximo = hpMaximo;
+        this.hpAtual = hpMaximo;
+    }
+    ataque() {
+        return 3;
+    }
+}
+//Mob Teste
+class MobTeste {
+    constructor(nome, hpMaximo) {
+        this.nome = nome;
+        this.hpMaximo = hpMaximo;
+        this.hpAtual = hpMaximo;
+    }
+    ataque() {
+        return 2;
+    }
+}
+// Função para criar o herói
+function criarHeroi() {
+    return new Heroi('Caic', 20);
+}
+// Função para criar o mobs
+function criarMob() {
+    return new MobTeste('Rato', 20);
+}
+// Função para atualizar a interface no HTML
+function atualizarInterface(jogador, inimigo) {
+    const elementoNomeJogador = document.getElementById('nome-personagem'); // Pega o elemento HTML
+    const elementoHpJogador = document.getElementById('hp-personagem'); // Pega o elemento HTML
+    const elementoNomeInimigo = document.getElementById('nome-inimigo'); // Pega o elemento HTML
+    const elementoHpInimigo = document.getElementById('hp-inimigo'); // Pega o elemento HTML
+    if (elementoNomeJogador) {
+        elementoNomeJogador.textContent = jogador.nome; // Atualiza o nome do personagem no HTML
+    }
+    if (elementoHpJogador) {
+        elementoHpJogador.textContent = `${jogador.hpAtual}/${jogador.hpMaximo}`; // Atualiza o hp do personagem
+    }
+    if (elementoNomeInimigo) {
+        elementoNomeInimigo.textContent = inimigo.nome; // Atualiza o nome do inimigo no HTML
+    }
+    if (elementoHpInimigo) {
+        elementoHpInimigo.textContent = `${inimigo.hpAtual}/${inimigo.hpMaximo}`; // Atualiza o hp do Inimigo
+    }
+}
+// Criando o herói
+const caic = criarHeroi();
+//Criando o Mob
+const mob1 = criarMob();
+// Chama a função para atualizar o nome do personagem quando a página for carregada
+atualizarInterface(caic, mob1);
+/* Primeiro contraolar quem inicia, para saber que vai atacar primeiro;
+A pessoa faz a ação dela, e isso invoca a função que ela escolher
+saindo o resultado do calculo, devemos ter 2 funcções await, que está esperando o HP do heroi ou do mob chegar a zero
+para determinar quem vai ganhar e perder;
+apos a primeiro pessoa jogar, a segunda joga, e quando a segunda jogar o turno 2 começa, até a luta acabar com o await*/
